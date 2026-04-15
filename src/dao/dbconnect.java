91343af03e4ec32d393535ba3ec28ec0c6b331f2
kkeiiy <aaxkeisha@gmail.com>
2026-04-15 20:09:45 +0700

    database

---
+++ src/dao/dbconnect.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbconnect {
+    private static final String URL = "jdbc:mysql://localhost:3307/photobooth";
+    private static final String USER = "root";
+    private static final String PASS = ""; 
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+
+/*public static void main(String[] args) {
+        try {
+            Connection c = getConnection();
+            if (c != null) {
+                System.out.println("tersambung");
+            }
+        } catch (Exception e) {
+            System.out.println("gagal" + e.getMessage());
+            e.printStackTrace();
+        }
+    }*/
+}
