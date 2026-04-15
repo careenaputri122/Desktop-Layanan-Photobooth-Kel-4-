cf49275eaf266e8fd8d57a87b254cb47721e351f
kkeiiy <aaxkeisha@gmail.com>
2026-04-15 21:02:58 +0700

    update

--- src/model/User.java
+++ src/model/User.java
@@ -3,14 +3,16 @@ package model;
 public class User {
     private int id;
     private String username;
+    private String email;
     private String password;
     private String role;
 
     public User() {}
 
-    public User(int id, String username, String password, String role) {
+    public User(int id, String username, String email, String password, String role) {
         this.id = id;
         this.username = username;
+        this.email = email;
         this.password = password;
         this.role = role;
     }
@@ -20,6 +22,9 @@ public class User {
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
